56ccc8c2061e9bb89de5825af829f2d7fbafc111
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2023-07-24 13:40:02 +0800

    bluebell 定义帖子投票功能相关的RedisKey

---
+++ dao/redis/keys.go
@@ -0,0 +1,12 @@
+package redis
+
+// redis key
+
+// redis key 注意使用命名空间的方式，方便查询和拆分
+
+const (
+	KeyPrefix              = "bluebell:"
+	KeyPostTimeZSet        = "post:time"   // zset; 帖子及发帖时间
+	KeyPostScoreZSet       = "post:score"  // zset; 帖子及投票的分数
+	KeyPostVotedZSetPrefix = "post:voted:" // zset; 记录用户及投票类型; 参数是 post ID
+)
